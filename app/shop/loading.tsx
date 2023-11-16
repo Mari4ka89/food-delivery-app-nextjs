608ff26ca2300423dfbd9e9eb27317deda7bd827
Mariia Bezrukava <mari4ka89@gmail.com>
2023-11-16 20:45:18 +0200

    Updated loading menu item loading skeleton

--- app/shop/loading.tsx
+++ app/shop/loading.tsx
@@ -1,11 +1,5 @@
-import SkeletonMenuItem from "components/SkeletonMenuItem";
+import { MenuItemsSkeleton } from "@/components/skeletons";
 
 export default function Loading() {
-  // TODO: display correct amount of skeletons
-  return (
-    <div className="grid grid-cols-2 gap-x-12 gap-y-8">
-      <SkeletonMenuItem />
-      <SkeletonMenuItem />
-    </div>
-  );
+  return <MenuItemsSkeleton />;
 }
---
+++ components/skeletons.tsx
@@ -0,0 +1,35 @@
+const shimmer =
+  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
+
+const MenuItemSkeleton = () => {
+  return (
+    <div
+      className={
+        "${shimmer} bordered p-4 relative overflow-hidden rounded-xl bg-white/5"
+      }
+    >
+      <div className="w-full h-80 rounded-lg bg-slate-100" />
+
+      <div className="flex justify-between pt-4">
+        <div className="w-3/5">
+          <div className="h-6 w-4/5 rounded-lg bg-slate-100" />
+          <div className="h-6 w-12 rounded-lg bg-slate-100" />
+        </div>
+        <div className="h-12 rounded-lg w-2/5 bg-slate-100" />
+      </div>
+    </div>
+  );
+};
+
+export const MenuItemsSkeleton = () => {
+  return (
+    <>
+      <MenuItemSkeleton />
+      <MenuItemSkeleton />
+      <MenuItemSkeleton />
+      <MenuItemSkeleton />
+    </>
+  );
+};
+
+export default MenuItemSkeleton;
